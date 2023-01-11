aac6484196bbc2d1f8135a6b6a474ed56229d1f0
mcc-yokoyama <106637304+mcc-yokoyama@users.noreply.github.com>
2023-01-11 11:24:07 +0900

    Add makefile for GNU make

---
+++ apps/rtos/freertos/secure_element_access/firmware/sam_e54_xpro.X/nbproject/Makefile-local-sam_e54_xpro.mk
@@ -0,0 +1,30 @@
+#
+# Generated Makefile - do not edit!
+#
+#
+# This file contains information about the location of compilers and other tools.
+# If you commmit this file into your revision control server, you will be able to 
+# to checkout the project and build it from the command line with make. However,
+# if more than one person works on the same project, then this file might show
+# conflicts since different users are bound to have compilers in different places.
+# In that case you might choose to not commit this file and let MPLAB X recreate this file
+# for each user. The disadvantage of not commiting this file is that you must run MPLAB X at
+# least once so the file gets created and the project can be built. Finally, you can also
+# avoid using this file at all if you are only building from the command line with make.
+# You can invoke make with the values of the macros:
+# $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
+#
+MP_CC="C:\Microchip\xc32\v4.20\bin\xc32-gcc.exe"
+MP_CPPC="C:\Microchip\xc32\v4.20\bin\xc32-g++.exe"
+MP_AS="C:\Microchip\xc32\v4.20\bin\xc32-as.exe"
+MP_LD="C:\Microchip\xc32\v4.20\bin\xc32-ld.exe"
+MP_AR="C:\Microchip\xc32\v4.20\bin\xc32-ar.exe"
+MP_BIN2HEX="C:\Microchip\xc32\v4.20\bin\xc32-bin2hex.exe"
+MP_OBJCOPY="C:\Microchip\xc32\v4.20\bin\xc32-objcopy.exe"
+MP_CC_DIR="C:\Microchip\xc32\v4.20\bin"
+MP_CPPC_DIR="C:\Microchip\xc32\v4.20\bin"
+MP_AS_DIR="C:\Microchip\xc32\v4.20\bin"
+MP_LD_DIR="C:\Microchip\xc32\v4.20\bin"
+MP_AR_DIR="C:\Microchip\xc32\v4.20\bin"
+# MP_BC_DIR is not defined
+CMSIS_DIR=../src/packs/CMSIS
