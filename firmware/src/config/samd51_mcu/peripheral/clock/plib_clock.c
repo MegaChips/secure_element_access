5a5a301c54639a6f55946dab1eeb1e062a801038
mccsawa <sawa.yuta@megachips.co.jp>
2023-06-23 13:43:24 +0900

    bugfix

--- firmware/src/config/samd51_mcu/peripheral/clock/plib_clock.c
+++ firmware/src/config/samd51_mcu/peripheral/clock/plib_clock.c
@@ -269,9 +269,9 @@ void CLOCK_Initialize (void)
         /* Wait for synchronization */
     }
 
-    /* Selection of the Generator and write Lock for SERCOM1_CORE as I2C */
-    GCLK_REGS->GCLK_PCHCTRL[8] = GCLK_PCHCTRL_GEN(0x3U) | GCLK_PCHCTRL_CHEN_Msk;
-    while ((GCLK_REGS->GCLK_PCHCTRL[8] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
+    /* Selection of the Generator and write Lock for SERCOM2_CORE as I2C */
+    GCLK_REGS->GCLK_PCHCTRL[23] = GCLK_PCHCTRL_GEN(0x3U)  | GCLK_PCHCTRL_CHEN_Msk;
+    while ((GCLK_REGS->GCLK_PCHCTRL[23] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
     {
         /* Wait for synchronization */
     }
